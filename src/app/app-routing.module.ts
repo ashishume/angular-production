073212f5554f29d3c9fab0da13bc98bea305cc0b
Ashish Debnath <ashish.debnath@nineleaps.com>
2019-07-10 10:40:30 +0530

    Routing fixed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,39 +6,28 @@ import { ProfileComponent } from './profile/profile.component';
 import { ProjectDetailsComponent } from './project-details/project-details.component';
 import { SignInComponent } from './auth/sign-in/sign-in.component';
 import { AuthGuardService } from './shared/auth-guard/auth-guard.service';
-import {PageNotFoundComponent} from  './page-not-found/page-not-found.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { TimelineComponent } from './timeline/timeline.component';
 import { SignupComponent } from './signup/signup.component';
 import { UsersComponent } from './users/users.component';
 import { UserDetailsComponent } from './user-details/user-details.component';
 import { SubmitProjectComponent } from './submit-project/submit-project.component';
 const routes: Routes = [
-  { path: '', component: SignInComponent},
-  {path:'mat-navbar',component:MatNavbarComponent},
-  {path:'dashboard',component:DashboardComponent},
-  {path:'profile',component:ProfileComponent},
-  {path:'project-details',component:ProjectDetailsComponent},
-  { path: 'timeline', component: TimelineComponent},
-  { path: 'users', component: UsersComponent},
-  { path: 'user-details', component: UserDetailsComponent},
-  { path: 'signup', component: SignupComponent },
-  { path: 'submit-project', component: SubmitProjectComponent },
-  { path: '**', component: PageNotFoundComponent },
-  // { path: '', component: SignInComponent},
-  // {path:'mat-navbar',component:MatNavbarComponent,canActivate:[AuthGuardService]},
-  // {path:'dashboard',component:DashboardComponent,canActivate:[AuthGuardService]},
-  // {path:'profile',component:ProfileComponent,canActivate:[AuthGuardService]},
-  // {path:'project-details',component:ProjectDetailsComponent,canActivate:[AuthGuardService]},
-  // { path: 'timeline', component: TimelineComponent,canActivate:[AuthGuardService] },
-  // { path: 'users', component: UsersComponent,canActivate:[AuthGuardService] },
-  // { path: 'user-details', component: UserDetailsComponent,canActivate:[AuthGuardService] },
-  // { path: 'signup', component: SignupComponent },
-  // { path: 'submit-project', component: SubmitProjectComponent },
-  // { path: '**', component: PageNotFoundComponent },
+	{ path: '', component: SignInComponent },
+	{ path: 'mat-navbar', component: MatNavbarComponent, canActivate: [ AuthGuardService ] },
+	{ path: 'dashboard', component: DashboardComponent, canActivate: [ AuthGuardService ] },
+	{ path: 'profile', component: ProfileComponent, canActivate: [ AuthGuardService ] },
+	{ path: 'project-details', component: ProjectDetailsComponent, canActivate: [ AuthGuardService ] },
+	{ path: 'timeline', component: TimelineComponent, canActivate: [ AuthGuardService ] },
+	{ path: 'users', component: UsersComponent, canActivate: [ AuthGuardService ] },
+	{ path: 'user-details', component: UserDetailsComponent, canActivate: [ AuthGuardService ] },
+	{ path: 'signup', component: SignupComponent },
+	{ path: 'submit-project', component: SubmitProjectComponent },
+	{ path: '**', component: PageNotFoundComponent }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+	imports: [ RouterModule.forRoot(routes) ],
+	exports: [ RouterModule ]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
